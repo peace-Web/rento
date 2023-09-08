99089f54095df11d6baea938727937c71e288cfe
mohssine <mohssineayane35@gmail.com>
2023-09-08 11:48:25 +0100

    custom domain

--- stateRent/settings.py
+++ stateRent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nh7+x2!j(qgi9o_#_ff-v8zwcj95jxtgezw!tt(m-marv!-uu7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
