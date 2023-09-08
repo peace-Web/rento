ba836232df584cf027ef6b32e80b33bec3e8bff0
mohssine <mohssineayane35@gmail.com>
2023-09-08 11:36:45 +0100

    modify settings

--- stateRent/settings.py
+++ stateRent/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-
-DATABASE_URL = 'postgresql://postgres:a6Cedf9xz4NNtp7YbtsL@containers-us-west-169.railway.app:7874/railway'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
