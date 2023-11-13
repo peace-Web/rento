e7b7942eb36954870336f830f3048d85e8803cfd
ayane <mohssineayane35@gmail.com>
2023-11-13 20:22:20 +0100

    update admin address

--- stateRent/settings.py
+++ stateRent/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nh7+x2!j(qgi9o_#_ff-v8zwcj95jxtgezw!tt(m-marv!-uu7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rento-production.up.railway.app"]
+ALLOWED_HOSTS = ["rento-production.up.railway.app", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ['https://rento-production.up.railway.app','https://127.0.0.1']
 # Application definition
